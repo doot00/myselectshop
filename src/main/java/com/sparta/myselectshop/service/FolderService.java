5c335b2b285dda57ef1cc6dc46216761a0974e22
doot00 <binnung_s@naver.com>
2024-11-04 19:18:48 +0900

    폴더 생성 및 조회 구현

---
+++ src/main/java/com/sparta/myselectshop/service/FolderService.java
@@ -0,0 +1,53 @@
+package com.sparta.myselectshop.service;
+
+import com.sparta.myselectshop.dto.FolderResponseDto;
+import com.sparta.myselectshop.entity.Folder;
+import com.sparta.myselectshop.entity.User;
+import com.sparta.myselectshop.repository.FolderRepository;
+import com.sun.nio.sctp.IllegalReceiveException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FolderService {
+    private final FolderRepository folderRepository;
+    public void addFolders(List<String> folderNames, User user) {
+        List<Folder> existFolderList = folderRepository.findAllByUserAndNameIn(user, folderNames);
+
+        List<Folder> folderList = new ArrayList<>();
+
+        for (String folderName : folderNames) {
+            if(!isExistFolderName(folderName, existFolderList)) {
+                Folder folder = new Folder(folderName, user);
+                folderList.add(folder);
+            } else {
+                throw new IllegalReceiveException("폴더명이 중복되었습니다.");
+            }
+        }
+        folderRepository.saveAll(folderList); // 받아온 폴더가 담긴다.
+    }
+
+    public List<FolderResponseDto> getFolders(User user) {
+        List<Folder> folderList = folderRepository.findAllByUser(user);
+        List<FolderResponseDto> responseDtoList = new ArrayList<>();
+
+        for (Folder folder : folderList) {
+            responseDtoList.add(new FolderResponseDto(folder)); // 객체에 하나씩 담기게 된다.
+        }
+        return responseDtoList;
+    }
+
+    private boolean isExistFolderName(String folderName, List<Folder> existFolderList) {
+        for (Folder existFolder : existFolderList) {
+            if(folderName.equals(existFolder.getName())) {
+                // 같은지 확인해야 한다.
+                return true;
+            }
+        }
+        return false; // 일치하는 것이 하나도 없을 때
+    }
+}
