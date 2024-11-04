10cd35c16db3f71ae036da051b8fb02f26151808
doot00 <binnung_s@naver.com>
2024-11-04 20:43:46 +0900

    폴더 별 관심상품 조회 구현
    폴더 별 관심상품 조회 구현

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -116,4 +116,16 @@ public class ProductService {
         productFolderRepository.save(new ProductFolder(product, folder)); // 최종적으로 데이터를 저장한다.
 
     }
+
+    public Page<ProductResponseDto> getProductsInFolder(Long folderId, int page, int size, String sortBy, boolean isAsc, User user) {
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        // 해당 폴더에 등록되어 있는 상품을 가져온다.
+        Page<Product> productList = productRepository.findAllByUserAndProductFolderList_FolderId(user, folderId, pageable);
+        Page<ProductResponseDto> responseDtoList = productList.map(ProductResponseDto::new);
+
+        return responseDtoList;
+    }
 }
